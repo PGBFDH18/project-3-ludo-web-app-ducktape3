0922ed3e6a9d62c9953117cf2fa6bd13cb74464b
Marcus Bernhard <Marcus.bernhard@outlook.com>
2019-02-12 10:39:20 +0100

    relogged github account test 3

--- src/CoreWebAPI/Startup.cs
+++ src/CoreWebAPI/Startup.cs
@@ -62,7 +62,7 @@ namespace CoreWebAPI
             });
             app.UseHttpsRedirection();
 
-            // Enable middleware to serve generated Swagger as s JSON endpoint.
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
             app.UseMvc();
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
