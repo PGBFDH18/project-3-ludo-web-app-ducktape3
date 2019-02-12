96a1e1288e3ccfd42c128ebecb6ec630e175f2ce
Marcus Bernhard <Marcus.bernhard@outlook.com>
2019-02-12 21:06:17 +0100

    Pushing from laptop

--- src/CoreWebAPI/Startup.cs
+++ src/CoreWebAPI/Startup.cs
@@ -62,7 +62,7 @@ namespace CoreWebAPI
             });
             app.UseHttpsRedirection();
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            // Enable middleware to serve generated Swagger as  JSON endpoint.
             app.UseSwagger();
             app.UseMvc();
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
